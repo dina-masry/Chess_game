6b3781af62dcb853163ed87ef0a9673a78214bb6
dina-masry <dina40012@gmail.com>
2026-06-28 13:28:27 +0300

    assignment 10

--- src/clean_chess.py
+++ src/clean_chess.py
@@ -5,15 +5,15 @@ def clean_chess(df):
     )
 
     # 2b — Add rating_diff
-    df['rating_diff'] = df['white_rating'] - df['black_rating']
+    df['rating_diff'] = abs(df['white_rating'] - df['black_rating'])
 
     # 2c — Extract opening_family
     df['opening_family'] = (
         df['opening_fullname'].str.split(':').str[0].str.strip()
     )
 
-    # 2d — Drop high-null column (~93.98% null)
-    df = df.drop(columns=['opening_response'])
+    # # 2d — Drop high-null column (~93.98% null)
+    # df = df.drop(columns=['opening_response'])
 
     # 2e — Flag short games
     df['is_suspicious'] = df['turns'] < 5   # 342 games
